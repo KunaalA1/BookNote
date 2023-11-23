35e7ec554a08ece54fc14b2b686c7c53f560573d
KunaalAenugula <kunaal.a1@gmail.com>
2023-11-23 15:53:11 -0800

    commit message test 3

---
+++ app.js
@@ -0,0 +1,28 @@
+const bookRoutes = require('./routes/bookRoutes');
+const dbURI = 'mongodb+srv://kaenugul:SparkTower@blogweb.6wicesc.mongodb.net/book_tracking?retryWrites=true&w=majority';
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const mongoose = require('mongoose');
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology:true}).then((result) => console.log('connected')).catch((err) => console.log(err));
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+app.get('/', (req, res) => {
+    res.redirect('/books');
+})
+
+app.use(bookRoutes);
+
+app.use((req, res) => {
+    res.status(404).render('404', {title: 'Page Not Found'});
+});
+
+app.listen(8080);
